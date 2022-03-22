5734454f521feba87ba3b500de2cadad9cd67c64
mujammelhoque <mujammelh111@gmail.com>
2022-03-22 23:11:54 +0600

    user data pass by factory tinker

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
